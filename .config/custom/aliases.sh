d86c300ad2438191c3d1535d792e0e6fa2854d86
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2024-09-19 14:46:54 +0530

    Adds alias for starting INDI server with drivers

--- .config/custom/aliases.sh
+++ .config/custom/aliases.sh
@@ -1,1 +1,3 @@
 git config --global alias.identity '! git config user.name "$(git config user.$1.name)"; git config user.email "$(git config user.$1.email)"; git config user.signingkey "$(git config user.$1.signingkey)"; :'
+
+alias indi-astropi='indiserver indi_asi_ccd indi_pmc8_telescope'
