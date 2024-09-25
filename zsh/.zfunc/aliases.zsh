e691aacd9081eb2885148b92c5618af360534992
Mayank Pathak <onlinejudge95@gmail.com>
2024-09-25 12:38:15 +0530

    Adds postgres service alias

--- zsh/.zfunc/aliases.zsh
+++ zsh/.zfunc/aliases.zsh
@@ -1,1 +1,2 @@
-alias wecoachdb="psql --host wecoach.cpq82ioic0rr.ap-south-1.rds.amazonaws.com --port 5432 --user postgres --database postgres --password"
+alias wecoachdb="psql --host wecoach.cpq82ioic0rr.ap-south-1.rds.amazonaws.com --port 5432 --user postgres --dbname postgres --password"
+alias dc-postgres="docker compose --file ~/.config/postgres/docker-compose.yml"
