4dc3ad49a2a033293ad8482f55014093ecd5a593
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2024-09-25 13:10:31 +0530

    Delete .config/custom directory

--- .config/custom/aliases.sh
+++
@@ -1,3 +0,0 @@
-git config --global alias.identity '! git config user.name "$(git config user.$1.name)"; git config user.email "$(git config user.$1.email)"; git config user.signingkey "$(git config user.$1.signingkey)"; :'
-
-alias indi-astropi='nohup indiserver indi_asi_ccd indi_asi_focuser indi_pmc8_telescope &'
