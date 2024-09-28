46b9db85495cadb9873c7b5a7af7faa6b600063a
Mayank Pathak <onlinejudge95@gmail.com>
2024-09-28 14:03:19 +0530

    Adds aliases for js development

--- zsh/.zfunc/aliases.zsh
+++ zsh/.zfunc/aliases.zsh
@@ -1,3 +1,4 @@
 alias wecoachdb="psql --host wecoach.cpq82ioic0rr.ap-south-1.rds.amazonaws.com --port 5432 --user postgres --dbname postgres --password"
 alias dc-postgres="docker compose --file ~/.config/postgres/docker-compose.yml"
-alias cna='npx create-next-app --typescript --tailwind --eslint --app --src-dir --use-yarn'
+alias cna="npx create-next-app --typescript --tailwind --eslint --app --use-yarn"
+alias yd="yarn lint && yarn format && yarn build && yarn dev"
