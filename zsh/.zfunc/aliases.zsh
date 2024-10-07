2e42369826765260355ce163174caba3f76981b4
Mayank Pathak <onlinejudge95@gmail.com>
2024-10-07 15:56:38 +0530

    Adds alias for helm

--- zsh/.zfunc/aliases.zsh
+++ zsh/.zfunc/aliases.zsh
@@ -2,3 +2,4 @@ alias wecoachdb="psql --host wecoach.cpq82ioic0rr.ap-south-1.rds.amazonaws.com -
 alias cna="npx create-next-app --typescript --tailwind --eslint --app --use-yarn"
 alias yd="yarn lint && yarn format && yarn build && yarn dev"
 alias kubectl="microk8s kubectl"
+alias helm="microk8s helm"
