4abb07afe3b7b521a78958af36a8cdd801e5ade6
Mayank Pathak <onlinejudge95@gmail.com>
2024-10-20 12:06:32 +0530

    Removes microk8s from alias

--- zsh/.zfunc/aliases.zsh
+++ zsh/.zfunc/aliases.zsh
@@ -1,5 +1,3 @@
 alias wecoachdb="psql --host wecoach.cpq82ioic0rr.ap-south-1.rds.amazonaws.com --port 5432 --user postgres --dbname postgres --password"
 alias cna="npx create-next-app --typescript --tailwind --eslint --app --use-yarn"
 alias yd="yarn lint && yarn format && yarn build && yarn dev"
-alias kubectl="microk8s kubectl"
-alias helm="microk8s helm"
