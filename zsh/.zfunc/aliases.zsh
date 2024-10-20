7ea64613bdeb31e341ee2b363cd05db35b24a805
Mayank Pathak <onlinejudge95@gmail.com>
2024-10-20 23:31:28 +0530

    Adds helm specific aliases

--- zsh/.zfunc/aliases.zsh
+++ zsh/.zfunc/aliases.zsh
@@ -2,4 +2,5 @@ alias wecoachdb="psql --host wecoach.cpq82ioic0rr.ap-south-1.rds.amazonaws.com -
 alias cna="npx create-next-app --typescript --tailwind --eslint --app --use-yarn"
 alias yd="yarn lint && yarn format && yarn build && yarn dev"
 alias kube-refresh="lxc file pull master/etc/kubernetes/admin.conf ~/.kube/config"
+alias h="helm"
 alias k="kubectl"
