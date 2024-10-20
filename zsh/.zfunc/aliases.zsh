941ee626b3a8606e1b33dad53fb382d6c1ae6a31
Mayank Pathak <onlinejudge95@gmail.com>
2024-10-20 23:30:05 +0530

    Adds kubectl specific aliases

--- zsh/.zfunc/aliases.zsh
+++ zsh/.zfunc/aliases.zsh
@@ -1,3 +1,5 @@
 alias wecoachdb="psql --host wecoach.cpq82ioic0rr.ap-south-1.rds.amazonaws.com --port 5432 --user postgres --dbname postgres --password"
 alias cna="npx create-next-app --typescript --tailwind --eslint --app --use-yarn"
 alias yd="yarn lint && yarn format && yarn build && yarn dev"
+alias kube-refresh="lxc file pull master/etc/kubernetes/admin.conf ~/.kube/config"
+alias k="kubectl"
