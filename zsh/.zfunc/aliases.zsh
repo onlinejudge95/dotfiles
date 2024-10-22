2cb486a1e33ffc1dd89237770a945656c6af5f10
Mayank Pathak <onlinejudge95@gmail.com>
2024-10-23 02:05:58 +0530

    Makes kube refresh a function

--- zsh/.zfunc/aliases.zsh
+++ zsh/.zfunc/aliases.zsh
@@ -1,7 +1,6 @@
 alias wecoachdb="psql --host wecoach.cpq82ioic0rr.ap-south-1.rds.amazonaws.com --port 5432 --user postgres --dbname postgres --password"
 alias cna="npx create-next-app --typescript --tailwind --eslint --app --use-yarn"
 alias yd="yarn lint && yarn format && yarn build && yarn dev"
-alias kube-refresh="lxc file pull kmaster/etc/kubernetes/admin.conf ~/.kube/config"
 alias h="helm"
 alias k="kubectl"
 alias icat="kitten icat"
--- zsh/.zfunc/functions.zsh
+++ zsh/.zfunc/functions.zsh
@@ -1,3 +1,9 @@
 seed_conf_directories () {
   mkdir --parents ~/.config/{ohmyposh,postgres,nvim/lua/custom}
 }
+
+krefresh() {
+  local node=$1
+
+  lxc file pull $node/etc/kubernetes/admin.conf ~/.kube/config
+}
