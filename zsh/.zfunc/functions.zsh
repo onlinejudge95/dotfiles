a202e006f2c9d12d164fd426da512e7f1891821b
Mayank Pathak <onlinejudge95@gmail.com>
2026-09-09 17:42:51 +0530

    feature: Adds postgresql config as docker compose file

--- zsh/.zfunc/functions.zsh
+++ zsh/.zfunc/functions.zsh
@@ -1,5 +1,5 @@
 seed_conf_directories () {
-  mkdir --parents ~/.config/{ohmyposh,postgres,nvim/lua/custom}
+  mkdir --parents ~/.config/{ohmyposh,postgres,nvim/lua/custom,postgresql}
 }
 
 krefresh() {
