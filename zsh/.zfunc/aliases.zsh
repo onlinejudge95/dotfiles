9eea0daee622e13fa182cc23bc556f3c0c8f8145
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2026-09-10 15:56:00 +0530

    feature: Install postgres (#17)

--- zsh/.zfunc/aliases.zsh
+++ zsh/.zfunc/aliases.zsh
@@ -13,6 +13,6 @@ alias dss="/opt/DeepSkyStacker/DeepSkyStacker"
 alias terraform="docker compose run --rm terraform"
 
 # PostgreSQL aliases
-alias createdb="docker compose --file ~/.config/postgresql/docker-compose.yaml exec postgres createdb"
-alias dropdb="docker compose --file ~/.config/postgresql/docker-compose.yaml exec postgres dropdb"
-alias psql="docker compose --file ~/.config/postgresql/docker-compose.yaml exec postgres psql"
+alias createdb="docker compose --file ~/.config/docker-compose/postgres.yaml exec postgres createdb"
+alias dropdb="docker compose --file ~/.config/docker-compose/postgres.yaml exec postgres dropdb"
+alias psql="docker compose --file ~/.config/docker-compose/postgres.yaml exec postgres psql"
