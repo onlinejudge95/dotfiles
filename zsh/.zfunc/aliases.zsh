f30f0e07c1f1fddbd395c11bf86f3dae93f9e737
Mayank Pathak <onlinejudge95@gmail.com>
2026-09-21 21:20:44 +0530

    feature: Adds alias for createdb and dropdb

--- zsh/.zfunc/aliases.zsh
+++ zsh/.zfunc/aliases.zsh
@@ -12,6 +12,8 @@ alias autostakkert="wine /usr/local/bin/AutoStakkert_4.0.11_x64/AutoStakkert.exe
 alias dss="/opt/DeepSkyStacker/DeepSkyStacker"
 
 # PostgreSQL aliases
+alias createdb="docker compose --file ~/.config/docker-compose/postgres.yaml run --interactive --rm --tty psql createdb"
+alias dropdb="docker compose --file ~/.config/docker-compose/postgres.yaml run --interactive --rm --tty psql dropdb"
 alias psql="docker compose --file ~/.config/docker-compose/postgres.yaml run --interactive --rm --tty psql psql"
 alias pgloader="docker compose --file ~/.config/docker-compose/postgres.yaml run --interactive --rm --tty pgloader pgloader"
 
